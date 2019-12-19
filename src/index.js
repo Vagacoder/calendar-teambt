cfd7eb1fae5b3b6d140f9ea6d02312a1c702c9b6
Vagacoder <qiruihu@gmail.com>
2019-12-19 14:50:30 -0800

    12/19 update week and day views' layout

--- src/index.js
+++ src/index.js
@@ -4,19 +4,48 @@ import Calendar from "./Calendar";
 const events = [
     {
         id: "123ABC456efg",
-        start: new Date().toISOString(),
+        start: new Date("2019-12-18T12:00:00").toISOString(),
         end: "",
-        title: "Event"
+        title: "Event 3"
+    },
+    {
+        id: "hj12UY89mL54",
+        start: new Date("2019-12-18T11:00:00").toISOString(),
+        end: "",
+        title: "Event 1"
     },
     {
         id: "abc543kji987",
-        start: new Date().toISOString(),
+        start: new Date("2019-12-18T11:30:00").toISOString(),
         end: "",
-        title: "Event"
-    }
+        title: "Event 2"
+    },
+    {
+        id: "yuRl683Ndejd2",
+        start: new Date("2019-12-18T12:30:00").toISOString(),
+        end: "",
+        title: "Event 4"
+    },
 ];
 
+const theme = {
+    palette: {
+        primary: {
+            light: "#568293",
+            main: "#285565",
+            dark: "#002c3b",
+            contrastText: "#fff"
+        },
+        secondary: {
+            light: "#ffa08c",
+            main: "#f86e5e",
+            dark: "#c03d34",
+            contrastText: "#000"
+        }
+    }
+}
+
 ReactDOM.render(
-    <Calendar events={events} onEventClick={(props) => { console.log(props) }} />,
+    <Calendar theme={theme} events={events} onEventClick={(props) => { console.log(props) }} />,
     document.getElementById("root")
 );
